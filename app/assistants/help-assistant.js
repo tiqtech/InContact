7aa26c7a00ff9da1a9ab5413c3f598b391a29d58
unknown <Ryan@.(none)>
2010-08-04 21:33:54 -0500

    v1.3.1 - (no back buttons ...)

--- app/assistants/help-assistant.js
+++ app/assistants/help-assistant.js
@@ -4,7 +4,6 @@ var HelpAssistant = Class.create(
 	},
 	setup:function() {
 		LBB.Util.loadTheme(this.controller);
-		this.controller.setupWidget(Mojo.Menu.commandMenu, {}, {visible: true, items: [{icon:"back-icon", command:'close-help'}]});
 	}
 });
 
